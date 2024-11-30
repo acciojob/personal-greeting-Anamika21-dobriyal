535a9a2a6c6d12e21924c51afe31ad6b2b19cd49
Anamika dobriyal <anamikadobriyal21@gmail.com>
2024-12-01 01:28:55 +0530

    3rd assignment of react

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  // Declare a state to store the user's name
+  const [name, setName] = useState('');
+
+  // Handle input change
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Personalized Greeting</h1>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={handleInputChange}
+        />
+        {name && <p>Hello, {name}!</p>}
     </div>
   )
 }
 
-export default App
+export default App;
+
