fa7b4478ec47536eb95ecce8e0bf3cf54b58f22c
Anamika <anamikadobriyal21@gmail.com>
2025-01-22 23:23:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,24 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  // Declare a state to store the user's name
-  const [name, setName] = useState('');
-
-  // Handle input change
-  const handleInputChange = (event) => {
-    setName(event.target.value);
-  };
-
+  const [input,setInput] = useState('');
+  const onInput = (e)=>{
+      const {value} = e.target
+    setInput(value)
+  }
+  const onClear = ()=>{
+    setInput('')
+  }
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>Personalized Greeting</h1>
-        <input
-          type="text"
-          placeholder="Enter your name"
-          value={name}
-          onChange={handleInputChange}
-        />
-        {name && <p>Hello, {name}!</p>}
+      <p>Enter your name:</p>
+    <input value={input} onChange={onInput} />
+    {!!input.length&&<p>Hello {input}!</p>}
+    <button onClick={onClear}>Clear</button>
     </div>
   )
 }
 
-export default App;
-
+export default App
